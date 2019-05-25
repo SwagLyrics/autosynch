404c7badec9f398fa380661c80ff3940104222e3
chriswang030 <cyw2124@columbia.edu>
2019-05-25 01:55:38 -0700

    LPF tests in C

--- c-tests/test.c
+++ c-tests/test.c
@@ -14,18 +14,42 @@ typedef struct {
 
 static void die(const char *s) { perror(s); exit(1); }
 
-static int callback(const void *input, void *output, unsigned long framecount, 
-        const PaStreamCallbackTimeInfo *timeinfo, PaStreamCallbackFlags statusflags, void *userdata) {
+//Need to fix: p_input & segfault
+sf_count_t lpf(void *output, unsigned long framecount,
+        callback_data_s *p_data, int cutoff) {
     
-    float *out = (float *)output;
+    sf_count_t nread;
+    float *p_input = malloc(sizeof(float) * framecount * p_data->info.channels); 
+    float *p_output = (float *)output;
+
+    double tau = 1.0 / cutoff;
+    double alpha = framecount / tau;
+
+    nread = sf_read_float(p_data->file, p_input, framecount * p_data->info.channels);
+     
+    float tmp = p_input[0];
+    for (int i = 0; i < sizeof(float) * framecount * p_data->info.channels; i++) {
+        tmp += alpha * (p_input[i]-tmp);
+        p_output[i] = tmp;
+    }
+
+    free(p_input);
+    return nread/2;
+}
+
+int callback(const void *input, void *output, unsigned long framecount, 
+        const PaStreamCallbackTimeInfo *timeinfo, PaStreamCallbackFlags statusflags,
+        void *userdata) {
+    
+    sf_count_t nread;
+    float *p_output = (float *)output;
     callback_data_s *p_data = (callback_data_s *)userdata;
-    sf_count_t num_read;
 
-    memset(out, 0, sizeof(float) * framecount * p_data->info.channels);
-    num_read = sf_read_float(p_data->file, out, framecount * p_data->info.channels);
-    num_read /= 2;
+    memset(p_output, 0, sizeof(float) * framecount * p_data->info.channels);
+    nread = lpf(output, framecount, p_data, 500);
+    //nread = sf_read_float(p_data->file, p_output, framecount * p_data->info.channels);
 
-    if (num_read < framecount)
+    if (nread < framecount)
         return paComplete;
     return paContinue;
 }
@@ -49,8 +73,8 @@ int main(int argc, char **argv) {
     if (Pa_Initialize() != paNoError)
         die("Pa_Initialize() failed");
 
-    if (Pa_OpenDefaultStream(&stream, 0, data.info.channels, paFloat32, data.info.samplerate,
-                FRAMES_PER_BUFFER, callback, &data) != paNoError)
+    if (Pa_OpenDefaultStream(&stream, 0, data.info.channels, paFloat32,
+                data.info.samplerate, FRAMES_PER_BUFFER, callback, &data) != paNoError)
         die("Pa_OpenDefaultStream() failed");
 
     if (Pa_StartStream(stream) != paNoError)
