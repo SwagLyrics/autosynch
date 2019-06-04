56dfde19b1f454c01e44198bed5c40489a61acdc
chriswang030 <cyw2124@columbia.edu>
2019-06-04 11:46:07 -0400

    Updated stripping algorithms

--- src/audio_comparator.py
+++ src/audio_comparator.py
@@ -1,33 +1,16 @@
-import numpy as np
-import os
-
-import config
-from syllable_extractor import SyllableExtractor
 import nussl
 
-test_dir = os.path.join(config.resourcesdir, 'Unprocessed')
-proc_dir = os.path.join(config.resourcesdir, 'Processed')
-
-extractor = SyllableExtractor()
-
-algorithms = []
-
-for algorithm in algorithms:
-    for file in os.listdir(test_dir):
-        file = os.path.join(test_dir, file)
-
-
-def ft2d(audio_signal):
+def ft2d(fpath):
     """Basic 2D Fourier transform vocal isolation method from nussl"""
 
-    signal = nussl.AudioSignal(audio_data_array=audio_signal)
+    signal = nussl.AudioSignal(fpath)
     ft2d = nussl.FT2D(signal)
     ft2d.run()
     _, foreground = ft2d.make_audio_signals()
 
-    return foreground
+    return [(foreground.sample_rate, foreground.audio_data)]
 
-def repet(audio_signal):
+def repet(fpath):
     """
     REPET original implementation.
 
@@ -37,14 +20,14 @@ def repet(audio_signal):
     Republic, May 22-27, 2011.
     """
 
-    signal = nussl.AudioSignal(audio_data_array=audio_signal)
+    signal = nussl.AudioSignal(fpath)
     repet = nussl.Repet(signal)
     repet.run()
     _, foreground = repet.make_audio_signals()
 
-    return foreground
+    return [(foreground.sample_rate, foreground.audio_data)]
 
-def repet_sim(audio_signal):
+def repet_sim(fpath):
     """
     REPET-SIM implementation.
 
@@ -53,14 +36,14 @@ def repet_sim(audio_signal):
     October 8-12, 2012.
     """
 
-    signal = nussl.AudioSignal(audio_data_array=audio_signal)
+    signal = nussl.AudioSignal(fpath)
     repet_sim = nussl.RepetSim(signal)
     repet_sim.run()
     _, foreground = repet_sim.make_audio_signals()
 
-    return foreground
+    return [(foreground.sample_rate, foreground.audio_data)]
 
-def hpss(audio_signal):
+def hpss(fpath):
     """
     LibROSA Harmonic/Percussive Source Separation (HPSS) implementation.
 
@@ -72,14 +55,14 @@ def hpss(audio_signal):
     Conference, Taipei, Taiwan, 2014.
     """
 
-    signal = nussl.AudioSignal(audio_data_array=audio_signal)
+    signal = nussl.AudioSignal(fpath)
     hpss = nussl.HPSS(signal)
     hpss.run()
     _, foreground = hpss.make_audio_signals()
 
-    return foreground
+    return [(foreground.sample_rate, foreground.audio_data)]
 
-def melodia(audio_signal):
+def melodia(fpath):
     """
     Melodia implementation.
 
@@ -91,14 +74,14 @@ def melodia(audio_signal):
     Language Processing, 20(6):1759-1770, Aug. 2012.
     """
 
-    signal = nussl.AudioSignal(audio_data_array=audio_signal)
+    signal = nussl.AudioSignal(fpath)
     melodia = nussl.Melodia(signal)
     melodia.run()
     _, foreground = melodia.make_audio_signals()
 
-    return foreground
+    return [(foreground.sample_rate, foreground.audio_data)]
 
-def duet(audio_signal, num_sources=1, dominance=False):
+def duet(fpath, num_sources=2, dominance=False):
     """
     DUET implementation.
 
@@ -109,31 +92,28 @@ def duet(audio_signal, num_sources=1, dominance=False):
     time-frequency masking," Signal Processing, IEEE Transactions on 52.7
     (2004): 1830-1847.
 
-    :param audio_signal: Audio signal must be stereophonic.
+    :param fpath: Audio signal must be stereophonic.
     :param num_sources: Number of foreground sources to extract.
     :param dominance: Whether one source is more dominant than another
     :return sources: List of audio signals with length num_sources
     """
-
-    if not audio_signal.is_stereo():
-        return None
 
     if dominance:
         p = 0.5
     else:
         p = 1
 
-    signal = nussl.AudioSignal(audio_data_array=audio_signal)
+    signal = nussl.AudioSignal(fpath)
+    if not signal.is_stereo:
+        return None
+
     duet = nussl.Duet(signal, num_sources, p=p)
     duet.run()
     sources = duet.make_audio_signals()
 
-    if num_sources == 1:
-        return sources[0]
-    else:
-        return sources
+    return [(source.sample_rate, source.audio_data) for source in sources]
 
-def projet(audio_signal, num_sources=1):
+def projet(fpath, num_sources=2):
     """
     PROJET implementation.
 
@@ -141,25 +121,22 @@ def projet(audio_signal, num_sources=1):
     Using Projections," 41st International Conference on Acoustics, Speech and
     Signal Processing, Shanghai, China, 2016.
 
-    :param audio_signal: Audio signal must be stereophonic.
+    :param fpath: Audio signal must be stereophonic.
     :param num_sources: Number of foreground sources to extract.
     :return sources: List of audio signals with length num_sources
     """
 
-    if not audio_signal.is_stereo():
+    signal = nussl.AudioSignal(fpath)
+    if not signal.is_stereo:
         return None
 
-    signal = nussl.AudioSignal(audio_data_array=audio_signal)
     projet = nussl.Projet(signal, num_sources)
     projet.run()
     sources = projet.make_audio_signals()
 
-    if num_sources == 1:
-        return sources[0]
-    else:
-        return sources
+    return [(source.sample_rate, source.audio_data) for source in sources]
 
-def nmf_mfcc(audio_signal, num_sources=1):
+def nmf_mfcc(fpath, num_sources=2):
     """
     Non-Negative Matrix Factorization using K-Means Clustering on Mel-frequency
     Cepstral Coefficients (NMF MFCC) implementation.
@@ -172,17 +149,14 @@ def nmf_mfcc(audio_signal, num_sources=1):
     :return sources: List of audio signals with length num_sources
     """
 
-    signal = nussl.AudioSignal(audio_data_array=audio_signal)
+    signal = nussl.AudioSignal(fpath)
     nmf_mfcc = nussl.NMF_MFCC(signal, num_sources)
     nmf_mfcc.run()
     sources = nmf_mfcc.make_audio_signals()
 
-    if num_sources == 1:
-        return sources[0]
-    else:
-        return sources
+    return [(source.sample_rate, source.audio_data) for source in sources]
 
-def rpca(audio_signal):
+def rpca(fpath):
     """
     Robust Principal Component Analysis (RPCA) implementation.
 
@@ -191,50 +165,9 @@ def rpca(audio_signal):
     Processing, IEEE International Conference, 2012.
     """
 
-    signal = nussl.AudioSignal(audio_data_array=audio_signal)
+    signal = nussl.AudioSignal(fpath)
     rpca = nussl.RPCA(signal)
     rpca.run()
     _, foreground = rpca.make_audio_signals()
 
-    return foreground
-
-def deep_clustering(audio_signal, num_sources=1):
-    """
-    Deep clustering implementation.
-
-    J. R. Hershey, Z. Chen, J. Le Roux, and S. Watanabe. "Deep clustering:
-    Discriminative embeddings for segmentation and separation," Acoustics,
-    Speech and Signal Processing, IEEE International Conference, 2016.
-
-    Y. Luo, Z. Chen, J. R. Hershey, J. Le Roux, and N. Mesgarani. "Deep
-    Clustering and Conventional Networks for Music Separation: Stronger
-    Together," arXiv, 2016.
-    """
-
-    signal = nussl.AudioSignal(audio_data_array=audio_signal)
-    dc = nussl.DeepClustering(signal, num_sources=num_sources)
-    dc.run()
-    _, foreground = dc.make_audio_signals()
-
-    return foreground
-
-
-
-
-# mix = '/Users/Chris/autosynch/resources/MSD100/Mixtures/Dev'
-# src = '/Users/Chris/autosynch/resources/MSD100/Sources/Dev'
-#
-# f1 = join(join(mix, 'BKS - Bulldozer'), 'mixture.wav')
-# f2 = join(join(src, 'BKS - Bulldozer'), 'vocals.wav')
-#
-# sr1, y1 = read(f1)
-# sr2, y2 = read(f2)
-#
-# bg = repet.sim(y1, sr1)
-# fg = y1 - bg
-#
-# fg = np.nan_to_num(fg)
-# y2 = np.nan_to_num(y2)
-#
-# dist, path = fastdtw(y2, y1, dist=euclidean)
-# print(dist)
+    return [(foreground.sample_rate, foreground.audio_data)]
--- src/syllable_extractor.py
+++ src/syllable_extractor.py
@@ -1,25 +1,26 @@
 import os
 import parselmouth as pm
 
-from config import parentdir, resourcesdir
+from config import resourcesdir
 
-class SyllableExtractor(object):
-    def __init__(self, silencedb=-25, mindip=2, minpause=0.3, showtext=2):
-        self.silencedb = silencedb
-        self.mindip = mindip
-        self.minpause = minpause
-        self.showtext = showtext
-        self.supported = ['.wav', '.aiff', '.aifc', '.au', '.mp3']
+supported = ['.wav', '.aiff', '.aifc', '.au', '.mp3']
 
-    def run(self, fpath):
-        fext = os.path.splitext(fpath)[-1]
-        if fext not in self.supported:
-            print('{} extension not supported'.format(fext))
+def run(fpath, silencedb=-25, mindip=2, minpause=0.3, showtext=2):
+    """
+    Runs PRAAT script to do syllable analysis on audio file.
 
-        with open(os.path.join(resourcesdir,'praat-script.txt'), 'r') as f:
-            script = f.read()
+    :param fpath: path of audio file
+    :param **kwargs: see praat-script.txt for keyword args description
+    :return: number of syllables
+    """
 
-        script = script.format(self.silencedb, self.mindip, self.minpause,
-                               self.showtext, fpath)
+    fext = os.path.splitext(fpath)[-1]
+    if fext not in supported:
+        print('{} extension not supported'.format(fext))
 
-        return pm.praat.run(script, capture_output=True)[1].strip().split()
+    with open(os.path.join(resourcesdir,'praat-script.txt'), 'r') as f:
+        script = f.read()
+
+    script = script.format(silencedb, mindip, minpause, showtext, fpath)
+
+    return pm.praat.run(script, capture_output=True)[1].strip()
