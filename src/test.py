60b05ad72ea1085f7417f30842d02297edd25204
chriswang030 <cyw2124@columbia.edu>
2019-06-10 04:08:46 -0400

    Added PyTorch and CommonFate algorithms

--- src/test.py
+++ src/test.py
@@ -72,52 +72,8 @@ def eval():
         lyrics = swaglyrics.cli.get_lyrics(track, artist, make_issue=False)
         print(lyrics)
 
-song = 'Marvin Gaye - I Heard It Through the Grapevine'
-
-eval_song = { 'name': song,
-              'lyric': -1,
-              'audio': { 'source': -1, 'test': [] } }
-
-# Get file paths
-path = os.path.join(data_dir, song)
-mix = os.path.join(path, 'mixture.wav')
-src = os.path.join(path, 'vocals.wav')
-
-# Get track information
-split_i = song.find('-')
-artist = song[:split_i-1]
-track = song[split_i+2:]
-
-print('{} - {}'.format(artist, track))
-print('---------------------------------------------------')
-
-# Do background strip and save as wav
-
-output = ac.msstorch(mix, apply_sparsity=False)
-for i, out in enumerate(output):
-    fout = os.path.join(path, '{}_test_{}.wav'.format(algorithm.__name__, i))
-    scipy.io.wavfile.write(fout, out[0], out[1].T)
-
-# Do audio syllable analysis
-eval_song['audio']['source'] = extractor.run(src)
-print('audio source:    {}'.format(eval_song['audio']['source']))
-
-for test in glob.glob(path + '/*test*.wav'):
-    file = os.path.split(test)[1]
-
-    try:
-        # Check bit depth compatibility
-        syl = extractor.run(test)
-    except PraatError:
-        # Use soundfile to convert file to PRAAT compatible bit depth
-        signal, sample_rate = soundfile.read(test)
-        soundfile.write(test, signal, sample_rate, subtype='PCM_16')
-        syl = extractor.run(test)
-
-    eval_song['audio']['test'].append(syl)
-    print('{}: {}'.format(file, eval_song['audio']['test'][-1]))
-
-# Get lyrics
-import swaglyrics.cli
-lyrics = swaglyrics.cli.get_lyrics(track, artist, make_issue=False)
-print(lyrics)
+from mss_pytorch import mss_pytorch
+from mss_pytorch.helpers import nnet_helpers
+sfiltnet = mss_pytorch.main(False, True)
+x, fs = soundfile.read('/Users/Chris/autosynch/src/mss_pytorch/results/test_files/test.wav')
+nnet_helpers.test_nnet(sfiltnet, x, fs, 60, 10*2, 2049, 4096, 384, 16)
