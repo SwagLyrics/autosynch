1fcde32fc9072c1b035a325962f1f70f761dbbf8
chriswang030 <cyw2124@columbia.edu>
2019-06-20 21:47:26 -0400

    Minor extraction adjustment

--- src/mad_twinnet_train/extract.py
+++ src/mad_twinnet_train/extract.py
@@ -2,34 +2,50 @@ import os
 import tarfile
 import yaml
 
-tar_path = '/home/cwang/MedleyDB_Sample.tar.gz'
-mixtures_dir = '/home/cwang/mad_twinnet_train/dataset/mixtures'
-sources_dir = '/home/cwang/mad_twinnet_train/dataset/sources'
+from scripts.training import training_process
+
+root_dir = '/Users/Chris/autosynch'
+tar_path = os.path.join(root_dir, 'resources/MedleyDB_V1.tar.gz')
+metadata_dir = os.path.join(root_dir, 'resources/Metadata')
+mixtures_dir = os.path.join(root_dir, 'src/mad_twinnet_train/dataset/mixtures')
+sources_dir = os.path.join(root_dir, 'src/mad_twinnet_train/dataset/sources')
+
+skip = dict.fromkeys(os.listdir(mixtures_dir))
 
 with tarfile.open(tar_path) as tar:
-    file = tar.next()
-    while file is not None:
-        basename = os.path.basename(file.name)
-        if basename.endswith('.yaml') and not basename.startswith('._'):
-            metadata = yaml.load(tar.extractfile(file).read().decode('utf-8'))
-            if metadata['excerpt'] == 'no' and metadata['has_bleed'] == 'no' and metadata['instrumental'] == 'no':
-                root_dir = os.path.dirname(file.name)
-                stem_dir = metadata['stem_dir']
-                mix_filename = metadata['mix_filename']
-                for stem in metadata['stems']:
-                    if metadata['stems'][stem]['component'] == 'melody':
-                        vox_filename = metadata['stems'][stem]['filename']
-                        break
+    for file in os.listdir(metadata_dir):
+        print(file)
+        file = os.path.join(metadata_dir, file)
 
-                mix_path = os.path.join(root_dir, mix_filename)
-                vox_path = os.path.join(os.path.join(root_dir, stem_dir), vox_filename)
+        with open(file, 'r') as f:
+            metadata = yaml.safe_load(f)
+        print('yaml loaded')
 
-                mix_file = tar.getmember(mix_path)
-                vox_file = tar.getmember(vox_path)
-                mix_file.name = os.path.basename(mix_path)
-                vox_file.name = os.path.basename(vox_path)
+        if metadata['has_bleed'] == 'no' and metadata['instrumental'] == 'no':
+            stem_dir = metadata['stem_dir']
+            base_dir = os.path.join('V1', stem_dir[:-6])
+            mix_filename = metadata['mix_filename']
 
-                tar.extract(mix_file, mixtures_dir)
-                tar.extract(vox_file, sources_dir)
+            if mix_filename in skip:
+                continue
 
-        file = tar.next()
+            for stem in metadata['stems']:
+                if metadata['stems'][stem]['component'] == 'melody' or 'rapper' in metadata['stems'][stem]['instrument']:
+                    vox_filename = metadata['stems'][stem]['filename']
+                    break
+
+            mix_path = os.path.join(base_dir, mix_filename)
+            vox_path = os.path.join(os.path.join(base_dir, stem_dir), vox_filename)
+
+            print('extracting')
+            mix_file = tar.getmember(mix_path)
+            vox_file = tar.getmember(vox_path)
+            mix_file.name = os.path.basename(mix_path)
+            vox_file.name = os.path.basename(vox_path)
+
+            tar.extract(mix_file, mixtures_dir)
+            tar.extract(vox_file, sources_dir)
+
+            print('done')
+
+training_process()
