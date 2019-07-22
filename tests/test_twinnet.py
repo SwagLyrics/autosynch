ef4c511bd7b56c9fac77a19aa0dfcc3df5dae588
chriswang030 <cyw2124@columbia.edu>
2019-07-21 19:04:24 -0700

    Tests for Travis

--- tests/test_twinnet.py
+++ tests/test_twinnet.py
@@ -21,10 +21,11 @@ def test_generate_output_path(audio_file):
     assert twinnet._make_target_file_names(['1&*!!.wav']) == [['1&*!!_voice.wav', '1&*!!_bg_music.wav']]
     assert twinnet._make_target_file_names(['1', '2']) == [['1_voice.wav', '1_bg_music.wav'], ['2_voice.wav', '2_bg_music.wav']]
 
-def test_twinnet_process_writes_to_file(audio_file, tmpdir):
-    """Requires `mad.pt` in `mad_twinnet/outputs/states` to work"""
-    out_base = os.path.splitext(os.path.basename(audio_file))[0] + '_voice.wav'
-    out_path = os.path.join(str(tmpdir), out_base)
-
-    assert twinnet.twinnet_process(audio_file, [[out_path, '']]) is True
-    assert os.path.isfile(out_path)
+# Commented out for Travis since `mad.pt` not in Github repo
+# def test_twinnet_process_writes_to_file(audio_file, tmpdir):
+#     """Requires `mad.pt` in `mad_twinnet/outputs/states` to work"""
+#     out_base = os.path.splitext(os.path.basename(audio_file))[0] + '_voice.wav'
+#     out_path = os.path.join(str(tmpdir), out_base)
+#
+#     assert twinnet.twinnet_process(audio_file, [[out_path, '']]) is True
+#     assert os.path.isfile(out_path)
